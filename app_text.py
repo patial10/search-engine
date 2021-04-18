9177cbff086f23321b54b00d8fd9d0288dc1ba61
patial10 <75527076+patial10@users.noreply.github.com>
2021-04-18 09:42:15 +0530

    Update app_text.py

--- app_text.py
+++ app_text.py
@@ -2,7 +2,7 @@ from flask import *
 import gensim
 import cv2
 import os
-model=gensim.models.Word2Vec.load("final_text_to_text.h5")
+# model=gensim.models.Word2Vec.load("final_text_to_text.h5")
 
 
 app = Flask(__name__)  
@@ -16,12 +16,12 @@ def upload():
 @app.route('/upload_image', methods = ['POST'])  
 def upload_image():   
     in_features = [x for x in request.form.values()]
-    prediction=model.wv.most_similar(positive=in_features, topn=5)
-    new_lst=[]
-    for i in prediction:
-        new_lst.append(i[0])  
+#     prediction=model.wv.most_similar(positive=in_features, topn=5)
+#     new_lst=[]
+#     for i in prediction:
+#         new_lst.append(i[0])  
         
-    return render_template("nlp_text.html", display = new_lst)  
+    return render_template("nlp_text.html", display = in_features)  
 
 if __name__ == '__main__':  
     app.run(debug = True) 
